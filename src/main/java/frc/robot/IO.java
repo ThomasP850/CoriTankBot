873350e4c27d2291432464c0426547dba251bc80
radicwl <marioxradical@gmail.com>
2022-10-09 14:20:22 -0400

    Setup custom buttons

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Button;
 
 /** Add your docs here. */
 
@@ -13,15 +14,42 @@ public class IO {
     private final int XBOX_PORT = 0;
     private final double DEADZONE = 0.05;
     private XboxController xbox;
+    private Button rightTriggerButton;
+    private Button leftTriggerButton;
+    private Button radialUp;
+    private Button radialRight;
+    private Button radialDown;
+    private Button radialLeft;
 
     public void init(){
         xbox = new XboxController(XBOX_PORT);
+        initializeCustomButtons();
     }
-
+    
     public double filter(double input){
         double x = Math.copySign(Math.pow(input, 2), input);
         return MathUtil.applyDeadband(x, DEADZONE);
     }
 
+    public void initializeCustomButtons(){
+        rightTriggerButton = new Button( () -> {
+            return xbox.getRightTriggerAxis()>0.5; 
+        } );
+        leftTriggerButton = new Button( () -> {
+            return xbox.getLeftTriggerAxis()>0.5;
+        } );
+        radialUp = new Button( () -> {
+            return xbox.getPOV()==0;
+        } );
+        radialRight = new Button( () -> {
+            return xbox.getPOV()==90;
+        } );
+        radialDown = new Button( () -> {
+            return xbox.getPOV()==180;
+        } );
+        radialLeft = new Button( () -> {
+            return xbox.getPOV()==270;
+        } );
+    }
 }
 
