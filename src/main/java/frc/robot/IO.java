04f657a9eabc878af2753dd94bd1845252e46f93
Araready <arreddy@us.ibm.com>
2022-10-09 13:30:59 -0400

    create IO class

---
+++ src/main/java/frc/robot/IO.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+
+/** Add your docs here. */
+
+public class IO {
+    private final int XBOX_PORT = 0;
+    private final double DEADZONE = 0.05;
+    private XboxController xbox;
+
+    public void init(){
+        xbox = new XboxController(XBOX_PORT);
+    }
+
+    public double filter(double input){
+        double x = Math.copySign(Math.pow(input, 2), input);
+        return MathUtil.applyDeadband(x, DEADZONE);
+    }
+
+}
+
